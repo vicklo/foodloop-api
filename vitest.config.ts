28cd81e4df424140a7323e7d288a3934cafb8267
victor <vic.kloeppel@gmail.com>
2022-12-15 15:04:37 +0100

    tsconfig deleted

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vitest/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   test: {
     includeSource: ['src/model/*.ts','/api/tsconfig.json'],
 
